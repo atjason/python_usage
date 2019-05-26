0694ddb3f5628f09920265bcde2520f0cfbbc7ee
Jason <quietjason@gmail.com>
2019-05-26 18:04:00 +0800

    Add random_demo.py

---
+++ utils/random_demo.py
@@ -0,0 +1,12 @@
+import random
+
+random.random() # [0.0, 1.0)
+random.uniform(1, 10) # [1, 10]
+
+random.choice([1, 2, 3])
+
+arr = [1, 2, 3]
+random.shuffle(arr)
+
+random.randrange(3)
+random.randrange(1, 10, 2)

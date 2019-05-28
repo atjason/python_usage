5f3eae93fc1be618043539f43e1a95da0122869c
Jason <quietjason@gmail.com>
2019-05-28 22:55:08 +0800

    Add aiohttp demo.

---
+++ net/aiohttp_demo.py
@@ -0,0 +1,61 @@
+import asyncio
+import aiohttp
+
+# Make a Request
+
+async def test_param(http_session):
+  params = {'key1': 'value1', 'key2': 'value2'}
+  headers = {'user-agent': 'my-app/0.0.1'}
+  url = 'https://postman-echo.com/get'
+  async with http_session.get(url, params=params, headers=headers) as resp:
+    resp.status
+    await resp.text()
+    j = await resp.json()
+    j['headers']['user-agent'] # 'my-app/0.0.1'
+
+async def test_post1(http_session):
+  data = {'key1': 'value1', 'key2': 'value2'}
+  headers = {'content-type': 'application/x-www-form-urlencoded'}
+  url = 'https://postman-echo.com/post'
+  async with http_session.post(url, headers=headers, data=data) as resp:
+    j = await resp.json()
+    str(j['json']) # "{'key1': 'value1', 'key2': 'value2'}"
+    j['headers']['content-type'] # 'application/x-www-form-urlencoded'
+
+async def test_post2(http_session):
+  data = {'key1': 'value1', 'key2': 'value2'}
+  url = 'https://postman-echo.com/post'
+  async with http_session.post(url, json=data) as resp:
+    j = await resp.json()
+    str(j['json']) # "{'key1': 'value1', 'key2': 'value2'}"
+    j['headers']['content-type'] # 'application/json'
+
+async def test_cookies(http_session):
+  cookies = {'key1': 'value1'}
+  url = 'https://postman-echo.com/cookies'
+  async with http_session.get(url, cookies=cookies) as resp:
+    print(await resp.text())
+    j = await resp.json()
+    j['cookies'] # '{"key1":"value1"}'
+
+async def test_timeout(http_session):
+  pass
+  # try:
+  #   url = 'https://postman-echo.com/cookies'
+  #   async with http_session.get(url, timeout=0.001) as resp:
+  #     pass
+  # except requests.exceptions.Timeout:
+  #   print('Timeout')
+
+async def main():
+  http_session = aiohttp.ClientSession()
+
+  await test_param(http_session)
+  await test_post1(http_session)
+  await test_post2(http_session)
+  await test_cookies(http_session)
+  await test_timeout(http_session)
+
+  await http_session.close()
+
+asyncio.run(main())

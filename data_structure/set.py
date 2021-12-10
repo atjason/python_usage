1d8cf5bdee209f1d704f654271d7fe4c51fbc142
Jason <quietjason@gmail.com>
2021-12-10 16:47:30 +0800

    Update set.py

--- data_structure/set.py
+++ data_structure/set.py
@@ -1,2 +1,13 @@
-s = set([1, 2, 3])
-s.add(4)
+
+s = set([1, 2])
+s.add(3)
+
+s.update([1, 2])
+s.update([1, 2], {1, 2})
+
+s.remove(3)
+# s.remove(4) # raise error
+s.discard(4) # do nothing
+
+len(s) # 3
+s.clear()

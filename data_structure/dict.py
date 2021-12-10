84bc0a156d293513686820909723be2b8423188f
Jason <quietjason@gmail.com>
2021-12-10 16:43:08 +0800

    Add dict.py

---
+++ data_structure/dict.py
@@ -0,0 +1,19 @@
+
+dict = {}
+dict = {'Name': 'Tom', 'Age': 3}
+
+dict['Name'] # 'Tom'
+
+# dict['Sex'] # raise error.
+dict.get('Sex') # None
+dict.get('Sex', 'No value') # 'No value'
+
+'Sex' in dict # False
+dict['Sex'] = 'Man' # // Set new key, or modify existing key.
+del dict['Sex']
+
+str(dict) # "{'Name': 'Tom', 'Age': 3}"
+
+dict.update({'Sex': 'Woman'}) # Merge 2 dict.
+
+dict.pop('Sex') # raise error has no such key.

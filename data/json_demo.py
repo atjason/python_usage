b53ce061ea9bbbb18bed852d364e8bd88303acf6
Jason <quietjason@gmail.com>
2021-12-11 08:06:23 +0800

    Update.

--- data/json_demo.py
+++ data/json_demo.py
@@ -8,7 +8,7 @@ j_str = json.dumps(j) # '{"name": "Tom", "age": 3}'
 json.loads(j_str) # {'name': 'Tom', 'age': 3}
 
 path = os.path.dirname(os.path.abspath(__file__))
-filepath = path + '/json_file.json'
+filepath = os.path.join(path, 'json_file.json')
   
 with open(filepath, 'w') as f:
   json.dump(j, f, indent=2)

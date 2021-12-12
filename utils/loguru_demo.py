95cda122ed0cc563a19eb3a5df428f5d90f0ebf5
Jason <quietjason@gmail.com>
2021-12-12 17:42:24 +0800

    Update loguru.

--- utils/loguru_demo.py
+++ utils/loguru_demo.py
@@ -8,6 +8,13 @@ logger.add("tmp/example.log", format="{time:MM-DD HH:mm:ss}| {level}| {message}"
 logger.debug("Log1")
 logger.error("Log2")
 
+def log(*args):
+  strs = [str(arg) for arg in args]
+  logger.info(' '.join(strs))
+
+log(1)
+log(1, 2)
+
 # @logger.catch
 # def f1():
 #   1/0

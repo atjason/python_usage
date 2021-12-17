0dd035d035a837f7f84d202130c551d88df1d0d6
Jason <quietjason@gmail.com>
2021-12-17 10:29:18 +0800

    Update datetime.

--- utils/date_time.py
+++ utils/date_time.py
@@ -1,6 +1,12 @@
 import time
+from datetime import datetime
 
 s = int(time.time())
 ms = int(time.time() * 1000)
 print(s)
 print(ms)
+
+now = datetime.now()
+now = datetime.fromtimestamp(now.timestamp())
+
+print(now.strftime("%Y%m%d %H:%M:%S"))	

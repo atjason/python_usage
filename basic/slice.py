e18686a56fa0f56f0a77cd763d1b00d7d503ab9d
Jason <quietjason@gmail.com>
2025-07-06 09:33:15 +0800

    Add slice.py

---
+++ basic/slice.py
@@ -0,0 +1,15 @@
+
+L = list(range(100))
+
+L[:10]  # first 10 elements
+L[-10:]  # last 10 elements
+L[10:20]  # elements from index 10 to 19
+L[::10]  # every 10th element
+L[::-1]  # reverse the list
+L[10:20:2]  # elements from index 10 to 19, step 2
+L[20:10:-1]  # elements from index 20 to 11, step -1
+
+L = 'ABCDEFG'
+
+L[:3] # 'ABC'
+L[-3:] # 'EFG'
